e11ae7391100b9c14d83a65e347e9ef5e20fc41c
Diego Avila <diegob.avila@hotmail.com>
2019-10-29 20:33:22 -0300

    Inputs criados e o component FormRow criado

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+import FormRow from '../components/FormRow';
 
 export default class LoginPage extends React.Component {
     render() {
         return (
             <View>
-                <Text>Loginn!!!!</Text>
+                <FormRow>
+                    <TextInput style={styles.input} placeholder= "Email"/>
+                </FormRow>
+                <FormRow>
+                    <TextInput style={styles.input} placeholder= "Senha" secureTextEntry />
+                </FormRow>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    input: {
+        paddingLeft:5,
+        paddingRight: 5,
+        paddingBottom:5,
+
+    }
+})
