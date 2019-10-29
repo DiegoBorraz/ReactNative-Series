4a4c525487a0e24a5c9051cfd2e892e92c727b61
Diego Avila <diegob.avila@hotmail.com>
2019-10-29 19:55:07 -0300

    Criado a pagina de navegação e o header default

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import LoginPage from './src/pages/LoginPage';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  'Login': {
+    screen: LoginPage,
+    navigationOptions: {
+      title: "Bem Vindo",
+    }
   },
+}, {
+  defaultNavigationOptions: {
+    title: "Series!",
+    headerTintColor: 'white',
+    headerStyle: {
+      backgroundColor: '#6ca2f7',
+      borderBottomCollor: '#C5C5C5',
+      borderBottomWidth: 1,
+    },
+    headerTitleStyle: {
+      color: 'white',
+      fontSize: 30,
+      flexGrow:1,
+      textAlign: 'center',
+    }
+  }
 });
+
+export default createAppContainer(AppNavigator);
