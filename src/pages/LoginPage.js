169dc554ac3f071e39413cef1a2f6b3ad58bfe6b
Diego Avila <diegob.avila@hotmail.com>
2019-10-30 10:46:26 -0300

    Foi criado o notão de login, está sendo setado no state os valores e correção nos estilos

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,23 +1,60 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View,  TextInput, StyleSheet, Button } from 'react-native';
 
 import FormRow from '../components/FormRow';
 
 export default class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    /*
+        Como o setState() não sabe o que esta recebendo é usado essa propriedade [] 
+        que assume o que esta sendo passado pelo parâmetro field e em seguida recebe o value
+        Uma forma pratica caso queira criar outros inputs
+    */
+    onChangeHandler(field, value){
+        this.setState({
+            [field]: value
+        })
+    }
+    tryLogin(){
+
+    }
+
     render() {
         return (
-            <View>
-                <FormRow>
-                    <TextInput style={styles.input} placeholder= "Email"/>
+            /* La no style do <FormRow/> será verificado se first ou last são true ou não para setar um estilo novo*/
+            <View style={styles.container}>
+                <FormRow first> 
+                    <TextInput 
+                    style={styles.input} 
+                    placeholder= "Email" 
+                    value={this.state.email}
+                    onChangeText={value =>this.onChangeHandler('email', value)}  />
                 </FormRow>
-                <FormRow>
-                    <TextInput style={styles.input} placeholder= "Senha" secureTextEntry />
+                <FormRow last> 
+                    <TextInput 
+                    style={styles.input} 
+                    placeholder= "Senha" 
+                    value={this.state.password} 
+                    onChangeText={value =>this.onChangeHandler('password', value)}
+                    secureTextEntry  />
                 </FormRow>
+                <Button title="Entrar" onPress={() => this.tryLogin()} />
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
+    container: {
+        paddingLeft: 10,
+        paddingRight: 10
+    },
     input: {
         paddingLeft:5,
         paddingRight: 5,
